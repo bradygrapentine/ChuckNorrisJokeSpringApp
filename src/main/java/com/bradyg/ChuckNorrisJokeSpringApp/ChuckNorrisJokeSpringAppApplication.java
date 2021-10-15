defc82b1dbe8dad95c84a4ae840a6f2abaa8a880
bradygrapentine <brady.grapentine@gmail.com>
2021-10-15 17:15:52 -0400

    initialized app

---
+++ src/main/java/com/bradyg/ChuckNorrisJokeSpringApp/ChuckNorrisJokeSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.bradyg.ChuckNorrisJokeSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokeSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokeSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bradyg/ChuckNorrisJokeSpringApp/ChuckNorrisJokeSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradyg.ChuckNorrisJokeSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
